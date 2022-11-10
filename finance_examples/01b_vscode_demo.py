d949560f27c854258a4130718f85da128c2ab7cb
iameskild <eskild@doublee.io>
2022-11-09 22:39:58 -0600

    Update vscode example, clean up

--- finance_examples/01b_vscode_demo.py
+++ finance_examples/01b_vscode_demo.py
@@ -2,18 +2,22 @@
 
 # Motivation: Show something we can only do with VS Code, for example: visaul debugging.
 
-import numpy as np
+import nasdaqdatalink
 import pandas as pd
 
-df = pd.DataFrame(
-    {
-        "A": ["foo", "bar", "foo", "bar", "foo", "bar", "foo", "foo"],
-        "B": ["one", "one", "two", "three", "two", "two", "three"],
-        "C": np.random.randn(8),
-        "D": np.random.randn(8),
-    }
-)
+import warnings
+warnings.filterwarnings("ignore")
 
-dfgb = df.groupby("A")[["C", "D"]].sum()
-
-print(dfgb)
+nasdaqdatalink.read_key(filename="~/shared/PyDataNYC/nasdaq_api_key")
+_x = 1e3
+date = pandas.DatetimeIndex(["2000-01-01"])[0]
+data = nasdaqdatalink.get("FRED/GDP")
+data["gdp_bil"] = data.Value.apply(lambda x_: x_/_x)
+gdp2000 = data.at[date].gdp_bil
+data["difference"] = data.gdp_bil.diff()
+big_drop_date, big_drop = data.difference.idxmin(), data.difference.min()
+dtstrfmt = '%B %Y'
+big_jump_date, big_jump = data.difference.idxmax(), data.difference.max()
+print(f"The largest quarter-on-quarter decrease was {big_drop;.4g} billion USD, recorded for the three months prior to {big_drop_date.strftime(dtstrfmt)}.")
+print(f"The largest quarter-on-quarter increase was {big_jump:.4g} billion USD, recorded for the three months prior to {big_jump_date.strftime(dtstrfmt)}.")
+print(f"The US GDP was {gdp2000:.4g} billon USD in {date.strftime(dtstrfmt)}.")
